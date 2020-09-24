7659252cffccbfba7d7b012f5d334c5f562ea833
unknown <yves.guyan@sklr.xyz>
2020-09-24 18:49:44 +0200

    split with templates

--- fbapp/views.py
+++ fbapp/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -9,9 +9,24 @@ app.config.from_object('config')
 @app.route('/')
 @app.route('/index/')
 def index():
-	return render_template('index.html')
-#	return "Hello world !"
+	description= """
+		Toi, tu sais comment utiliser la console ! Jamais à court d'idées pour réaliser ton objectif, tu es déterminé-e et persévérant-e. Tes amis disent d'ailleurs volontiers que tu as du caractère et que tu ne te laisses pas marcher sur les pieds. Un peu hacker sur les bords, tu aimes trouver des solutions à tout problème. N'aurais-tu pas un petit problème d'autorité ? ;-)
+	"""
+	return render_template('index.html',
+	                        user_name="Tom",
+							user_image=url_for('static', filename='/img/profile.png'),
+							description=description,
+							blur =True)
 
+@app.route('/result/')
+def result():
+	description = """
+        Toi, tu n'as pas peur d'être seul ! Les grands espaces et les aventures sont faits pour toi. D'ailleurs, Koh Lanta est ton émission préférée ! Bientôt tu partiras les cheveux au vent sur ton radeau. Tu es aussi un idéaliste chevronné. Quelle chance !
+	"""
+	return render_template('result.html',
+						    user_name='Tom',
+							user_image=url_for('static', filename='tmp/cover_111823112767411.jpg'),
+							description=description)
 
 # if __name__ == "__main__":
 # 	app.run()
